46aa77c8ecc6955979304e33dd92e85a137d03d3
twój login do GitHuba <96842769+MaksBernatowicz@users.noreply.github.com>
2022-10-25 17:24:43 +0200

    finished Header with Nav and Navmobile

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import React from 'react';
+import React from "react";
 
 // import aos
-import Aos from 'aos';
-import 'aos/dist/aos.css';
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 // import components
-import Banner from './components/Banner';
-import Header from './components/Header';
-import About from './components/About';
-import Workouts from './components/Workouts';
-import Pricing from './components/Pricing';
-import Community from './components/Community';
-import Faq from './components/Faq';
-import Join from './components/Join';
-import Footer from './components/Footer';
+import Banner from "./components/Banner";
+import Header from "./components/Header";
+import About from "./components/About";
+import Workouts from "./components/Workouts";
+import Pricing from "./components/Pricing";
+import Community from "./components/Community";
+import Faq from "./components/Faq";
+import Join from "./components/Join";
+import Footer from "./components/Footer";
 
 const App = () => {
   // aos initialization
   Aos.init({
     duration: 2500,
     delay: 400,
-  })
+  });
   return (
-    <div className='max-w-[1440px] mx-auto bg-page overflow-hidden relative'>
+    <div className="max-w-[1440px] mx-auto bg-page overflow-hidden relative">
       <Header />
       <Banner />
       <About />
