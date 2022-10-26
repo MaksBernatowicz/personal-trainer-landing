04b306994da419ed58e615758276dfeea23e02a9
twój login do GitHuba <96842769+MaksBernatowicz@users.noreply.github.com>
2022-10-26 23:56:12 +0200

    finished Workouts section with Slider

--- src/components/Workouts.js
+++ src/components/Workouts.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React from "react";
+
+// import data
+import { workouts } from "../data";
+
+// import components
+import WorkoutSlider from "../components/WorkoutSlider";
 
 const Workouts = () => {
-  return <div>Workouts</div>;
+  // destructure workouts data
+  const { title, icon } = workouts;
+  return (
+    <section className="section">
+      {/* section title */}
+      <div
+        className="section-title-group max-w-[540px] mx-auto px-4 lg:px-0"
+        data-aos="fade-up"
+        data-aos-delay="200"
+      >
+        <img src={icon} alt="" />
+        <h2 className="h2 section-title">
+          {title} <span className="text-primary-200">.</span>
+        </h2>
+      </div>
+      {/* slider */}
+      <div data-aos="fade-up" data-aos-delay="300">
+        <WorkoutSlider />
+      </div>
+    </section>
+  );
 };
 
 export default Workouts;
